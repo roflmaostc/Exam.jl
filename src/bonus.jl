9526f0863f32ff9551de2ce04ce15d22eba56e7b
roflmaostc <fxw+git@mailbox.org>
2022-02-25 23:38:50 +0100

    Bonus only if grade is passed [skip ci]

--- src/bonus.jl
+++ src/bonus.jl
@@ -2,14 +2,22 @@
     increment_grade(grades, bonuses, exam)
 
 One way of apply a bonus. We simply increment the grade by one level.
+But only if the `grade` is `passed => true`.
 """
 function increment_grade(grades, bonuses, exam)
     all_possible_grades = get_grades(exam) 
 
+    grades_bonus_eligible = []
+    for g in exam["grades"]
+        if g["passed"]
+            push!(grades_bonus_eligible, g["grade"])
+        end
+    end
+
     new_grades = []
     for (grade, bonus) in zip(grades, bonuses)
         ind = findfirst(g -> g == grade, all_possible_grades)
-        if ind > 1 && bonus
+        if ind > 1 && bonus && grade ∈ grades_bonus_eligible
             push!(new_grades, all_possible_grades[ind-1])
         else
             push!(new_grades, grade)
